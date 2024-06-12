48826a602720740c52bd81067d75dece2af4aea5
Daniel Evangelista <159594075+Evangelistabv@users.noreply.github.com>
2024-06-11 21:00:23 -0600

    Update settings.py

--- APIs/settings.py
+++ APIs/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -106,7 +106,8 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 AUTH_USER_MODEL = 'users.UserData'
-
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
