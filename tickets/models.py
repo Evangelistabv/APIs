570f52a7369f69dca5e46cccf96e5f6dd512cfdf
Daniel Evangelista <159594075+Evangelistabv@users.noreply.github.com>
2024-06-14 13:27:13 -0600

    Update models.py

--- tickets/models.py
+++ tickets/models.py
@@ -16,4 +16,4 @@ class Purchase(models.Model):
     total_product = models.DecimalField(max_digits=10, decimal_places=2)
 
     def __str__(self):
-        return f"{self.product} - {self.customer.id}"
+        return f"{self.product}"
